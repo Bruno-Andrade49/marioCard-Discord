e3ebeaf8370bf78190d3cf4989cd069241812146
Bruno Andrade <b.emanueandrade@hotmail.com>
2022-02-01 15:18:16 -0300

    Primeiro Commit

---
+++ pages/index.js
@@ -0,0 +1,234 @@
+import appConfig from "../config.json";
+// JavaScript
+import { Button, Box, Text, TextField, Image } from '@skynexui/components';
+
+function GlobalStyle() {
+    return (
+        <style global jsx>{`
+      * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+        list-style: none;
+      }
+      body {
+        font-family: 'Open Sans', sans-serif;
+      }
+      /* App fit Height */ 
+      html, body, #__next {
+        min-height: 100vh;
+        display: flex;
+        flex: 1;
+      }
+      #__next {
+        flex: 1;
+      }
+      #__next > * {
+        flex: 1;
+      }
+      /* ./App fit Height */ 
+    `}</style>
+    );
+}
+
+
+
+function Title(props) {
+    console.log(props.children)
+    const Tag = props.tag || "h1";
+    return (
+        <>
+            <Tag> {props.children} </Tag>
+
+            <style jsx>{`
+
+            ${Tag} {
+                color: ${appConfig.theme.colors.neutrals['400']};
+                font-size: 24px;
+                font=weight: 600;
+            }
+            `}</style>
+
+        </>
+    );
+}
+
+
+/*function HomePage() {
+    return (
+        //JSX
+        <div>
+            <NewName>Boas vindas de volta!</NewName>
+            <h2> Discord - MatrixPosts</h2>
+            <Button
+                iconName="github"
+                label="Button Component"
+            />
+            <Box
+                as=""
+                styleSheet={{
+                    backgroundColor: {
+                        lg: 'purple',
+                        md: 'green',
+                        sm: 'blue',
+                        xl: 'black',
+                        xs: 'red'
+                    },
+                    color: 'white',
+                    margin: '16px',
+                    padding: '16px'
+                }}
+            >
+                Box Component
+            </Box>
+
+        </div>
+
+
+    )
+}
+*/
+
+function PaginaInicial() {
+    const username = 'Bruno-Andrade49';
+    
+
+    return (
+        <>
+            <GlobalStyle />
+
+            <Box
+                styleSheet={{
+                    display: "flex", alignItems: "center", justifyContent: "center",
+                    backgroundColor: appConfig.theme.colors.neutrals["400"],
+                    backgroundImage: 'url(https://virtualbackgrounds.site/wp-content/uploads/2020/07/super-mario-bros-level-ending.jpg)',
+                    backgroundRepeat: "no-repeat", backgroundSize: "cover", backgroundBlendMode: 'multiply',
+                }}
+            >
+                <Box
+                    styleSheet={{
+                        display: "flex",
+                        alignItems: "center",
+                        justifyContent: "center",
+                        flexDirection: {
+                            xs: "column",
+                            sm: "row"
+                        },
+
+
+                        width: "100%", maxWidth: "700px",
+                        borderRadius: "5px", paddin: "32px", margin: "16px",
+                        boxShadow: "0 2px 10px 0 rgb(0 0 0  / 20%)",
+                        backgroundColor: appConfig.theme.colors.neutrals[700]
+                    }}
+                >
+                <Box
+                    as="form"
+                    styleSheet={{
+                        display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center',
+                        width: { xs: '100%', sm: '50%' }, textAlign: 'center', marginBottom: '32px',
+                    }}
+
+                 >
+                    <Title tag="h2">Bem vindos de volta, ao meu Discord!</Title>
+                    <Text variant="body3" styleSheet={{ marginBottom: '32px', color: appConfig.theme.colors.neutrals[300] }}>
+                        {appConfig.name}
+                    </Text>
+
+                    <TextField
+                        fullWidth
+                        textFieldColors={{
+                            neutral: {
+                            textColor: appConfig.theme.colors.neutrals[200],
+                            mainColor: appConfig.theme.colors.neutrals[900],
+                            mainColorHighlight: appConfig.theme.colors.primary[500],
+                            backgroundColor: appConfig.theme.colors.neutrals[800],
+                            },
+                        }}
+                    />
+
+                    <Button
+                        type='submit'
+                        label='Entrar'
+                        fullWidth
+                        buttonColors={{
+                            contrastColor: appConfig.theme.colors.neutrals["000"],
+                            mainColor: appConfig.theme.colors.primary['400'],
+                            mainColorLight: appConfig.theme.colors.primary[400],
+                            mainColorStrong: appConfig.theme.colors.primary[600],
+                        }}
+                    />
+                    </Box>
+                    <Box
+                        styleSheet={{
+                            display: 'flex',
+                            flexDirection: 'column',
+                            alignItems: 'center',
+                            maxWidth: '200px',
+                            padding: '16px',
+                            backgroundColor: appConfig.theme.colors.neutrals[800],
+                            border: '1px solid',
+                            borderColor: appConfig.theme.colors.neutrals[999],
+                            borderRadius: '10px',
+                            flex: 1,
+                            minHeight: '240px',
+                            position: 'relative',
+                            left: '60px'
+                    }}
+                    >
+                    <Image
+                        styleSheet={{
+                            borderRadius: '50%',
+                            marginBottom: '16px',
+                        }}
+                            src={`https://github.com/${username}.png`}
+                        />
+                    <Text
+                        variant="body4"
+                        styleSheet={{
+                            color: appConfig.theme.colors.neutrals[200],
+                            backgroundColor: appConfig.theme.colors.neutrals[900],
+                            padding: '3px 10px',
+                            borderRadius: '1000px'
+                        }}
+                    >
+                        {'Bruno Andrade'}
+                    </Text>
+                    </Box>
+                    </Box>
+            </Box>
+
+
+
+
+
+
+
+        </>
+    );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+export default PaginaInicial;
