eeb18bc4561a3fd7fed6b93bef08219dc8444f94
Bruno Andrade <b.emanueandrade@hotmail.com>
2022-02-01 15:42:07 -0300

    .

---
+++ .next/server/pages/index02.js
@@ -0,0 +1,24 @@
+(() => {
+var exports = {};
+exports.id = 847;
+exports.ids = [847];
+exports.modules = {
+
+/***/ 184:
+/***/ (() => {
+
+
+
+/***/ })
+
+};
+;
+
+// load runtime
+var __webpack_require__ = require("../webpack-runtime.js");
+__webpack_require__.C(exports);
+var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+var __webpack_exports__ = (__webpack_exec__(184));
+module.exports = __webpack_exports__;
+
+})();
---
+++ .next/static/chunks/pages/index02-66db31d04570bb2d.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[847],{1121:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/index02",function(){return u(2184)}])},2184:function(){}},function(n){n.O(0,[888,774,179],(function(){return _=1121,n(n.s=_);var _}));var _=n.O();_N_E=_}]);
